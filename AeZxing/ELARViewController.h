93ff73f614cf3d83e087a003b52963e88e3cfde5
Ruben Hernando <hernando.ruben@gmail.com>
2013-02-20 00:03:01 +0100

    Initial Commit

---
+++ AeZxing/ELARViewController.h
@@ -0,0 +1,13 @@
+//
+//  ELARViewController.h
+//  AeZxing
+//
+//  Created by Rubén Hernando Martín on 20/02/13.
+//  Copyright (c) 2013 Rubén Hernando Martín. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELARViewController : UIViewController
+
+@end
---
+++ AeZxing/ELARViewController.m
@@ -0,0 +1,29 @@
+//
+//  ELARViewController.m
+//  AeZxing
+//
+//  Created by Rubén Hernando Martín on 20/02/13.
+//  Copyright (c) 2013 Rubén Hernando Martín. All rights reserved.
+//
+
+#import "ELARViewController.h"
+
+@interface ELARViewController ()
+
+@end
+
+@implementation ELARViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
